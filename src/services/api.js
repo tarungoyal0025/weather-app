b9f4b52f0c41f3d9e82b3dea18f4349d74440e26
tarungoyal0025 <125543703+tarungoyal0025@users.noreply.github.com>
2023-07-23 19:50:04 +0530

    final commit

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const API_KEY = '5029c31b637cef730f324dc7635dfc92';
+const API_URL = 'http://api.openweathermap.org/data/2.5/weather';
+
+export const getWeather = async (city, country) => {
+    try {
+        let response = await axios.get(`${API_URL}?q=${city},${country}&appid=${API_KEY}&units=metric`);
+        return response.data;
+    } catch (error) {
+        console.log('Error while calling the api ', error.message);
+        return error.response;
+    }
+}
